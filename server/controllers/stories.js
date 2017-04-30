6ab1f796cdf0444a664101923eb6ec2fcdd6d03c
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-04-30 21:48:23 +1000

    rewrite index function to get all documents

--- server/controllers/stories.js
+++ server/controllers/stories.js
@@ -4,13 +4,11 @@ const collection = db.get('stories')
 
 module.exports = {
   index (req, res) {
-    collection.find({}, {}, function (e, docs) {
+    collection.find({}).then(function (docs) {
       console.log(docs)
       console.log('received request')
       res.status(200).json(docs)
     })
-    // res.status(200).json('return all stories')
-    // res.send('return all stories')
   },
   create (req, res) {
     // create a story here
