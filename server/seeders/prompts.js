73b8957d1a343c09cba4ee3442200d0d01128016
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-04-30 21:45:26 +1000

    seed prompts

---
+++ server/seeders/prompts.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    'prompt': 'Who has been the most important person of your life?'
+  },
+  {
+    'prompt': 'What are you grateful for today?'
+  },
+  {
+    'prompt': 'What did not go well today?'
+  },
+  {
+    'prompt': 'What is your earliest memory?'
+  },
+  {
+    'prompt': 'When in life have you felt alone?'
+  },
+  {
+    'prompt': 'What are your hopes for the future?'
+  },
+  {
+    'prompt': 'Do you have any regrets?'
+  },
+  {
+    'prompt': 'What was your childhood like?'
+  },
+  {
+    'prompt': 'How would your friends remember you?'
+  }
+]
