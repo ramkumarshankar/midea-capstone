32e232ccde50cbbdfeff5139d7dd88a58ae8e368
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-05-01 18:05:17 +1000

    seed some stories

---
+++ server/seeders/stories.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    'prompt': 'What is your earliest memory?',
+    'text': 'Getting in trouble at school for not coming to class',
+    'enabled': true
+  },
+  {
+    'prompt': 'What did not go well today?',
+    'text': 'I really could have prepared a bit better for the quiz.',
+    'enabled': true
+  },
+  {
+    'prompt': 'How would your friends remember you?',
+    'text': "Well, I can only hope they think I'm kind and helpful.",
+    'enabled': false
+  }
+]
