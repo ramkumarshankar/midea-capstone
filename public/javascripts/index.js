03c5a3171a45f27f9cb34327589994b54cb143b5
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-05-01 18:50:02 +1000

    animate background and setup vue

---
+++ public/javascripts/index.js
@@ -0,0 +1,12 @@
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: "{{{ title }}}"
+//   },
+//   components: {
+//     'heading': {
+//       props: ['message'],
+//       template: "<h1>{{ message }}</h1>"
+//     }
+//   }
+// })
