27d3610c58af363c95379a879138bd81487ef37b
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-05-01 18:04:17 +1000

    only select enabled records

--- server/controllers/stories.js
+++ server/controllers/stories.js
@@ -4,7 +4,7 @@ const collection = db.get('stories')
 
 module.exports = {
   index (req, res) {
-    collection.find({}).then(function (docs) {
+    collection.find({'enabled': true}).then(function (docs) {
       console.log(docs)
       console.log('received request')
       res.status(200).json(docs)
