fd7157ee080e8bdad11c589e474b01aba51276b6
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-05-01 21:28:52 +1000

    move vue.js to separate file

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,25 @@
-// var app = new Vue({
-//   el: '#app',
-//   data: {
-//     message: "{{{ title }}}"
-//   },
-//   components: {
-//     'heading': {
-//       props: ['message'],
-//       template: "<h1>{{ message }}</h1>"
-//     }
-//   }
-// })
+var app = new Vue({
+  el: '#app',
+  data: {
+    currentIndex: 0,
+    bShowStory: true,
+    stories: data,
+    prompt: "",
+    story: ""
+  },
+  components: {
+    'heading': {
+      props: ['prompt', 'story'],
+      template: "<div class='story'><h3>{{ prompt }}</h3><p>{{ story }}</p></div>"
+    }
+  },
+  created: function() {
+    this.updateStory(0);
+  },
+  methods: {
+    updateStory: function (index) {
+      this.prompt = this.stories[index].prompt;
+      this.story = this.stories[index].text;
+    }
+  }
+});
