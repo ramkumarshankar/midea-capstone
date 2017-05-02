8eed96c4d556f251a48c93ba240192ef9e4a95a4
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-05-02 16:18:32 +1000

    cycle through prompts

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -4,30 +4,30 @@ var app = new Vue({
     currentIndex: 0,
     prompts: prompts,
     currentPrompt: "",
-    bShowPrompt: false
+    bShowPrompt: true
   },
   components: {
     'prompt': {
       props: ['prompt'],
-      template: "<transition name='slidefade'><div><div class='row'><a id='refreshBtn' v-on:click='emitRefresh' class='button button-clear float-left' href='#'>Refresh Prompt</a></div><div class='row'><h4>{{ prompt }}</h4></div></div></transition>",
+      template: "<transition name='promptfade' v-on:leave='emitRefresh' v-on:after-leave='emitShow'><div class='row'><h4>{{ prompt }}</h4></div></transition>",
       methods: {
-        emitRefresh: function() {
-          this.$emit('refresh-prompt');
+        emitRefresh: function(el, done) {
+          this.$emit('refresh-prompt')
+          done()
+        },
+        emitShow: function(el) {
+          console.log('emit show');
+          this.$emit('show-prompt');
         }
       }
     }
   },
   created: function() {
-    // this.updateStory(0)
-    this.hidePrompt();
-    this.changePrompt();
-    this.showPrompt();
+    this.currentPrompt = prompts[Math.floor(Math.random() * this.prompts.length)].prompt;
   },
   methods: {
     changePrompt: function () {
-      this.hidePrompt();
       this.currentPrompt = prompts[Math.floor(Math.random() * this.prompts.length)].prompt;
-      this.showPrompt();
     },
     hidePrompt: function () {
       this.bShowPrompt = false;
