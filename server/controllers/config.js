9028d4b0c5d2f324a52ef9194383e9f1076bbf7c
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-05-06 19:41:38 +1000

    heroku mongodb

--- server/controllers/config.js
+++ server/controllers/config.js
@@ -1,10 +1,10 @@
 // mongod --dbpath ./data
 
-var mongoUri = process.env.MONGOHQ_URL || "localhost:27017/capstone";
+var mongoUri = MONGODB_URI || "localhost:27017/capstone";
 
 const mongo = require('mongodb')
 const monk = require('monk')
 // const db = monk('localhost:27017/capstone')
-const db = monk(process.env.MONGOHQ_URL)
+const db = monk(mongoUri)
 
 module.exports = db
