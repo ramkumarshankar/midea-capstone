f8daefd3a7acd6637eeb43b667c6fb14a5a3e536
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-05-06 19:49:53 +1000

    quick and dirty update for heroku

--- server/http-common.js
+++ server/http-common.js
@@ -1,7 +1,8 @@
 const axios = require('axios')
 
 const HTTP = axios.create({
-  baseURL: `http://localhost:8000/`
+  // baseURL: 'http://localhost:8000/'
+  baseURL: 'http://story-wall.herokuapp.com/'
 })
 
 module.exports = HTTP
