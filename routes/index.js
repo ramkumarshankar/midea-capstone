c0c69cec1b79e9631be22522f1a7e40649d35237
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-05-07 23:48:36 +1000

    use heroku webroot if deployed

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 var socketURL = process.env.HEROKU_SOCKET_URL || 'http://localhost:3000/'
+var webURL = (process.env.HEROKU_URL || 'http://localhost:8000/') + 'post'
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -9,6 +10,7 @@ router.get('/', function (req, res, next) {
     req.HTTP.get('/api/stories').then(function (stories) {
       res.render('index', {
         title: 'Home Page',
+        postURL: webURL,
         socketio: socketURL,
         prompts: JSON.stringify(prompts.data),
         stories: JSON.stringify(stories.data)
