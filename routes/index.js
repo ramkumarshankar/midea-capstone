bda931264b2027eabc6ca14f20e536361cd6c2d2
Ramkumar Shankar <ramkumarshankr@gmail.com>
2017-05-07 17:06:13 +1000

    get all stories and prompts when loading

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,16 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  req.HTTP.get('/api/stories').then(function (response) {
-    console.log(response.data)
-    res.render('index', {
-      title: 'Home Page',
-      data: JSON.stringify(response.data)
+  req.HTTP.get('/api/prompts').then(function (prompts) {
+    console.log(prompts.data)
+    req.HTTP.get('/api/stories').then(function (stories) {
+      console.log(stories.data)
+      res.render('index', {
+        title: 'Home Page',
+        // data: JSON.stringify(output)
+        prompts: JSON.stringify(prompts.data),
+        stories: JSON.stringify(stories.data)
+      })
     })
   }, function (err) {
     console.error('error getting stories: ')
